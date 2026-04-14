f09eb1ea3ed360533a963a776446a5bc4ef075ac
._. <143967879+Vlone111@users.noreply.github.com>
2026-04-15 04:16:09 +0500

    Update jade.sh

--- jade.sh
+++ jade.sh
@@ -83,6 +83,7 @@ ufw allow $NODE_PORT
 #Буст
 echo "net.core.default_qdisc=fq" >>/etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.conf
+sysctl -p
 echo "nameserver 1.1.1.1"
 
 #DNS
